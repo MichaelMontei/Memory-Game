a4dfac2d9705c537e8e82fa154352d6429977e3c
MichaelMontei <code-me@hotmail.com>
2022-05-20 15:18:00 +0200

    added function: having problems with the cards that are turned over when matched they dont return to the original position.

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
+
+//select all the cards
 const cards = document.querySelectorAll(".cards");
 
+//function to flip all cards with a forEach
 function flipCard() {
     this.classList.toggle("flip");
 }
 cards.forEach((card) => card.addEventListener("click", flipCard));
 
+let lockBoard;
+let firstCard;
+let secondCard;
+
+function getMatch(){
+    if(firstCard === secondCard){
+        lockBoard = true;
+    }else if(firstCard != secondCard){
+        lockBoard = false;
+        unFlipCards();
+    }
+}
+
+function unFlipCards(){
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+}, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let ramdomPos = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPos;
+    });
+})();
